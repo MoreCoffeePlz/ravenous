93588fad946c90434f3b6570a5d0d84569f7ba4c
Nicole <sandford.nicole@gmail.com>
2017-12-25 13:38:23 -0500

    message

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,8 +1,9 @@
 const apiKey = '0jUnTEdbmZMexqY6lUl1VAAdCoq6FMtlFkx-S-QoOfiABNRNYZOPpmVxxOlUhKGTQgXN1j-iUC35FaSCOn0u62NwrzcaaNGNOiQweNqLDHPNOIeT2zPIsaWhxDlBWnYx'
 const Yelp = {
   search(term, location, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, { headers: {
-    { Authorization: `Bearer ${apiKey}` }
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+     Authorization: `Bearer ${apiKey}` 
   }
 }).then(response => {
   response.json();
